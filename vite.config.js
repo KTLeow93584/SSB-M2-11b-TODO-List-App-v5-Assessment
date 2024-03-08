2dec09a88fa9dbaaae85564e259726dd8e69411d
Leow Kean Tat <leowkeantat@gmail.com>
2024-03-08 10:11:14 +0800

    Temporarily removed "base" key for Vercel upload purposes.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SSB-M2-11b-TODO-List-App-v5-Assessment/"
+  //base: "/SSB-M2-11b-TODO-List-App-v5-Assessment/"
 })
