1776403e39a22f72dc452a968c048a1ecc7d1272
Leow Kean Tat <leowkeantat@gmail.com>
2024-03-08 21:38:47 +0800

    Removed GitHub Pages setup from the main branch.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  //base: "/SSB-M2-11b-TODO-List-App-v5-Assessment/"
+  plugins: [react()]
 })
